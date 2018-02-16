db5a514fcd058ec447558459ab73d0b1951b73b0
Jason Hart <megadrago88@gmail.com>
2018-02-15 21:05:52 -0500

    starrted work on layout

--- Guibari-CPP/Guibari-CPP/Main/pregui/Source/startup.cpp
+++ Guibari-CPP/Guibari-CPP/Main/pregui/Source/startup.cpp
@@ -4,7 +4,7 @@ Summery
 Starts logger
 Checks current Guibari dir for {Startup_settings.(?XML??TXT?)} file and/or log folder
 if not present runs {Firsttime.cpp} and/or creates log folder
-if present checks for errors (Change in dir, ver mismatch, etc.) and throws them
+if present checks for errors (Change in dir, version mismatch, etc.) and throws them
 if there are no errors starts the gui
 */
 
